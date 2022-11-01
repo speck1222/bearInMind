5f8b82665bed30507bd5909eb07b76f186f47090
Spencer Peck <speck@repay.com>
2022-11-01 11:56:18 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("http://srv-captain--bear-in-mind-backend/api")
+    fetch("https://srv-captain--bear-in-mind-backend/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
