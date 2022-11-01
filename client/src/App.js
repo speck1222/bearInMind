32a835d3d3a0fbad4b66da7cd3cfd1c2bcccb0c8
Spencer Peck <speck@repay.com>
2022-11-01 11:26:42 -0500

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("https://bear-in-mind-backend.game.peckappbearmind.com/api")
+    fetch("http://srv-captain--bear-in-mind-backend/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
