e97743c0ffc90c6f64a27bdabf10c6bbb4bdb943
speck1222 <speck@repay.com>
2022-12-16 19:41:49 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("https://srv-captain--bear-in-mind-backend/api")
+    fetch("http://srv-captain--bear-in-mind-backend/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
