1e829a1ae1534eece4fffbe4c271a559f55748b0
speck1222 <speck@repay.com>
2022-12-16 19:48:26 -0600

    commit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
