7019fa2e50639f88fbd054460994484254bf6272
speck1222 <speck@repay.com>
2022-12-16 20:30:26 -0600

    cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 // server/index.js
 const path = require('path');
 const express = require("express");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
