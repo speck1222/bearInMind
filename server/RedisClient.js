649a2314d8496e23aca393963c87449eafc36547
speck1222 <speck@repay.com>
2022-12-18 13:08:33 -0600

    commit

--- server/RedisClient.js
+++ server/RedisClient.js
@@ -2,7 +2,14 @@ const redis = require('redis')
 
 function getRedisClient() {
     // const client = process.env.NODE_ENV === 'production' ? redis.createClient(6379, 'srv-captain--game-instance' , {password: 'Dalltx13'}) : redis.createClient(6379, '127.0.0.1')
-    const client = redis.createClient(6379, 'srv-captain--game-instance' , {password: 'Dalltx13'})
+    // const client = redis.createClient(6379, 'srv-captain--game-instance' , {password: 'Dalltx13'})
+    const client = redis.createClient({
+        socket: {
+            host: 'srv-captain--game-instance',
+            port: 6379,
+        },
+        password: 'Dalltx13'
+    })
     client.on('error', (err) => console.log('Redis Client Error', err));
     return client
 }
